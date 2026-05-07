864ce65bd790db7ef178c94fb33ffe068c45b039
aliakarma <443059463@stu.iu.edu.sa>
2026-05-07 17:45:30 +0300

    Update __init__.py

--- fl/baselines/__init__.py
+++ fl/baselines/__init__.py
@@ -3,6 +3,8 @@ from fl.baselines.krum_strategy import KrumStrategy
 from fl.baselines.trimmed_mean_strategy import TrimmedMeanStrategy
 from fl.baselines.fltrust_strategy import FLTrustStrategy
 from fl.baselines.foolsgold_strategy import FoolsGoldStrategy
+from fl.baselines.flame_strategy import FLAMEStrategy
+from fl.baselines.rfa_strategy import RFAStrategy
 
 __all__ = [
     "FedAvgStrategy",
@@ -10,4 +12,6 @@ __all__ = [
     "TrimmedMeanStrategy",
     "FLTrustStrategy",
     "FoolsGoldStrategy",
+    "FLAMEStrategy",
+    "RFAStrategy",
 ]
